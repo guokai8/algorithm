670f0e0dec27970f41ba512d9c763be2b522c55b
bioguo <guokai8@gmail.com>
2020-04-23 20:42:47 -0500

    Update decision_tree.R

--- decision_tree.R
+++ decision_tree.R
@@ -1,13 +1,15 @@
 
 #特征A对训练数据集D的信息增益g(D|A)定义为集合D的经验熵H(D)与特征更A给定条件下D的经验条件熵H(D|A)之差，即：
-#g(D,A)=H(D)−H(D|A)熵越大，说明系统越混乱，携带的信息就越少。熵越小，说明系统越有序，携带的信息就越多。信息的作用就是在于消除不确定性。
-#H(D)=-sum(p*log2p)
-#H(D|A)= 
-#ID3划分特征使用的就是信息增益IG。一个属性的信息增益越大，表明属性对样本的熵减少的能力就更强，该属性使得数据所属类别的不确定性变为确定性的能力越强。
+# g(D,A)=H(D)−H(D|A)熵越大，说明系统越混乱，携带的信息就越少。熵越小，说明系统越有序，携带的信息就越多。信息的作用就是在于消除不确定性。
+# H(D)=-sum(p*log2p)
+# H(D|A) first split by A then calculate H(D)
+# ID3划分特征使用的就是信息增益IG。一个属性的信息增益越大，表明属性对样本的熵减少的能力就更强，该属性使得数据所属类别的不确定性变为确定性的能力越强。
 # 在决策树中，ID3属性划分标准使用的是信息增益，C4.5使用的是信息增益率。
 # C4.5算法继承了ID3算法的优点，并在以下几方面对ID3算法进行了改进：能够对不完整数据进行处理。
-# C4.5算法有如下优点：产生的分类规则易于理解，准确率较高。其缺点是：在构造树的过程中，需要对数据集进行多次的顺序扫描和排序，因而导致算法的低效。另外，C4.5只适合于能够驻留于内存的数据集，当训练集大得无法在内存容纳时程序无法运行。
-# 另外，无论是ID3还是C4.5最好在小数据集上使用，决策树分类一般只试用于小数据。当属性取值很多时最好选择C4.5算法，ID3得出的效果会非常差，因为使用信息增益划分时它倾向于取值多的属性。
+# C4.5算法有如下优点：产生的分类规则易于理解，准确率较高。其缺点是：在构造树的过程中，需要对数据集进行多次的顺序扫描和排序，
+# 因而导致算法的低效。另外，C4.5只适合于能够驻留于内存的数据集，当训练集大得无法在内存容纳时程序无法运行。
+# 另外，无论是ID3还是C4.5最好在小数据集上使用，决策树分类一般只试用于小数据。当属性取值很多时最好选择C4.5算法，ID3得出的效果会非常差，
+# 因为使用信息增益划分时它倾向于取值多的属性。
 
 ### calculate H(D) -(sample in each Class/total sample)*log2(sample in each Class/total sample)
 hd<-function(train){
@@ -65,8 +67,10 @@ returnlabel <- function(train) {
     label
 }
 ##### 
-#### step 1 
-                          
+#### case 1: if only one class return class name
+#### case 2: if only one type A, return majority class
+### case 3: check ID4 of C4.5 info increase , if large the cutoff do create_tree 
+###    if small than cuoff return majority class                      
 create_tree <- function(train, func, err){
     nc <- ncol(train)
     labels <- colnames(train)
