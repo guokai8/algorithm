fcfe3f6a2d4c26699841df1030f771602d810e65
bioguo <guokai8@gmail.com>
2020-04-23 20:36:34 -0500

    Update decision_tree.R

--- decision_tree.R
+++ decision_tree.R
@@ -1,3 +1,15 @@
+
+#特征A对训练数据集D的信息增益g(D|A)定义为集合D的经验熵H(D)与特征更A给定条件下D的经验条件熵H(D|A)之差，即：
+#g(D,A)=H(D)−H(D|A)熵越大，说明系统越混乱，携带的信息就越少。熵越小，说明系统越有序，携带的信息就越多。信息的作用就是在于消除不确定性。
+#H(D)=-sum(p*log2p)
+#H(D|A)= 
+#ID3划分特征使用的就是信息增益IG。一个属性的信息增益越大，表明属性对样本的熵减少的能力就更强，该属性使得数据所属类别的不确定性变为确定性的能力越强。
+# 在决策树中，ID3属性划分标准使用的是信息增益，C4.5使用的是信息增益率。
+# C4.5算法继承了ID3算法的优点，并在以下几方面对ID3算法进行了改进：能够对不完整数据进行处理。
+# C4.5算法有如下优点：产生的分类规则易于理解，准确率较高。其缺点是：在构造树的过程中，需要对数据集进行多次的顺序扫描和排序，因而导致算法的低效。另外，C4.5只适合于能够驻留于内存的数据集，当训练集大得无法在内存容纳时程序无法运行。
+# 另外，无论是ID3还是C4.5最好在小数据集上使用，决策树分类一般只试用于小数据。当属性取值很多时最好选择C4.5算法，ID3得出的效果会非常差，因为使用信息增益划分时它倾向于取值多的属性。
+
+### calculate H(D) -(sample in each Class/total sample)*log2(sample in each Class/total sample)
 hd<-function(train){
     nc <- ncol(train)
     colnames(train)[nc]<-"class"
@@ -10,6 +22,7 @@ hd<-function(train){
     res <- sum(prob$plogp)
     res
 }
+### calculate H(D|A) A is one of the class category. 
 hda <- function(train,select){
     nr <- nrow(train)
     nc <- ncol(train)
@@ -24,6 +37,7 @@ hda <- function(train,select){
     #res <- sum(prob$pd)
     res
 }
+#ID3                         
 info_gain<-function(train,select){
     info<-hd(train)-hda(train,select)
     info
@@ -36,7 +50,7 @@ ahd <- function(train,select){
     res <- hd(traina)
     res
 }
-###
+###C4.5
 info_gain_ratio <- function(train,select){
     nominator <- hd(train) - hda(train,select)
     denominator <- ahd(train,select)
@@ -50,8 +64,9 @@ returnlabel <- function(train) {
     label <- names(tab)[idx_max]
     label
 }
-#####
-
+##### 
+#### step 1 
+                          
 create_tree <- function(train, func, err){
     nc <- ncol(train)
     labels <- colnames(train)
@@ -87,3 +102,4 @@ create_tree <- function(train, func, err){
     }
     }
 }
+                         
